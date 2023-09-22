ae0d8e5f3af57f913bf42f9051622565010be747
GabbiGmz <138724785+gabbigmz@users.noreply.github.com>
2023-09-22 09:55:19 -0400

    Update Gabbi's PacMen script.js
    Collab info

--- Gabbi's PacMen script.js
+++ Gabbi's PacMen script.js
@@ -1,6 +1,8 @@
 var pos = 0;
-var pacArray = ['PacMan1.png', 'PacMan2.png', 'PacMan3.png', 'PacMan4.png'];
-var direction = 0;
+var pacArray = [
+    ['PacMan1.png', 'PacMan2.png'], 
+    ['PacMan3.png', 'PacMan4.png'],
+];
 const pacMen = [];
 
 function setToRandom(scale) {
@@ -10,47 +12,68 @@ function setToRandom(scale) {
     }
 }
 
-
 // PacMan Factory
 function makePac() {
     let velocity = setToRandom(10);
     let position = setToRandom(550);
+    let direction = parseInt(Math.random()*2);
+    let focus = parseInt(Math.random()*2);
+
+    if (direction === 1) {
+        velocity.x = -velocity.x;
+    }
+
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
-    //newimg.src = pacArray[Math.floor(Math.random()*pacArray.length)]; 
-    newimg.src = "PacMan1.png"
+    newimg.src = pacArray[direction][focus];
     newimg.width = 100;
-    newimg.style.left = position.x;
-    newimg.style.top = position.y;
+    newimg.style.left = position.x + "px";
+    newimg.style.top = position.y + "px";
+
+
     game.appendChild(newimg);
     return {
         position,
         velocity,
-        newimg
-    }
+        newimg,
+        direction,
+        focus
+    };
 }
 
 function update() {
     pacMen.forEach((item) => {
-        checkCollisions(item)
+        checkCollisions(item);
+        item.focus = (item.focus + 1) % 2;
+        item.newimg.src = pacArray[item.direction][item.focus];
+
         item.position.x += item.velocity.x;
         item.position.y += item.velocity.y;
 
         item.newimg.style.left = item.position.x;
         item.newimg.style.top = item.position.y;
-    })
-    setTimeout(update, 20);
+    });
+    setTimeout(update, 60);
 }
 
 function checkCollisions(item) {
-    if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
-        item.position.x + item.velocity.x < 0) item.velocity.x = -item.velocity.x;
-    if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
-        item.position.y + item.velocity.y < 0) item.velocity.y = -item.velocity.y;
+    if (item.position.x + item.velocity.x + item.newimg.width >= window.innerWidth ||
+        item.position.x + item.velocity.x <= 0
+        ) { 
+        item.velocity.x = -item.velocity.x;
+        item.direction = (item.direction + 1) % 2;
+        }
+    if (item.position.y + item.velocity.y + item.newimg.height >= window.innerHeight ||
+        item.position.y + item.velocity.y <= 0
+        ) { 
+        item.velocity.y = -item.velocity.y;
+        }
 }
 
 function makeOne() {
     pacMen.push(makePac()); // This will add a new PacMan
 }
+
+// Project collab with MIT + Women Cohort 2023 classmates: Jackeline Park, Jennifer Eldridge, Gabriela Gomez, Candace Ryan, Helga Sallaku, Jesica Hurstle & Sarah Zimmerman.
 
